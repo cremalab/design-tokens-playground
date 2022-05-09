547089e95e35d4db38e7e50988525f6900341f84
Blake Tarter <blakertarter@gmail.com>
2022-05-09 17:00:34 -0500

    Add tailwind and design-token example

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -6,7 +6,9 @@ export function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        {/**   defined in variables.css-|  hint for tailwind-| */}
+        {/**margin-top-▼                ▼   text-color-▼     ▼ */}
+        <p className="mt-[var(--size-spacing-large)] text-[color:var(--color-base-gray-dark)]">
           Edit <code>src/components/App/index.tsx</code> and save to reload.
         </p>
         <a
