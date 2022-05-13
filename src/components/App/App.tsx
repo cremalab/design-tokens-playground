bc8ea777ba5c83945c0532f3f4d70628b107a119
Blake Tarter <blakertarter@gmail.com>
2022-05-13 14:53:28 -0500

    Add a better component example

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -6,9 +6,7 @@ export function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        {/**   defined in variables.css-|  hint for tailwind-| */}
-        {/**margin-top-▼                ▼   text-color-▼     ▼ */}
-        <p className="mt-[var(--size-spacing-large)] text-[color:var(--color-base-gray-dark)]">
+        <p>
           Edit <code>src/components/App/index.tsx</code> and save to reload.
         </p>
         <a
---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,30 @@
+import classNames from "classnames"
+import { ButtonHTMLAttributes, DetailedHTMLProps, MouseEventHandler, ReactChild } from "react"
+
+type ButtonProps = DetailedHTMLProps<
+  ButtonHTMLAttributes<HTMLButtonElement>,
+  HTMLButtonElement
+>
+
+interface Props {
+  buttonProps?: ButtonProps
+  children?: ReactChild
+  className?: string
+  onClick?: MouseEventHandler<HTMLButtonElement>
+}
+
+export function Button({ children, className, onClick, buttonProps = {} }: Props) {
+  return (
+    <button
+      {...buttonProps}
+      onClick={onClick}
+      className={classNames(
+        "py-[var(--size-spacing-small)] px-[var(--size-spacing-medium)] bg-[color:var(--color-base-gray-medium)] text-[color:var(--color-base-gray-dark)] hover:shadow-lg hover:bg-[color:var(--color-base-gray-light)]",
+        "rounded focus:ring transition duration-200",
+        className,
+      )}
+    >
+      {children}
+    </button>
+  )
+}
