37608fe0bd99b00d4da14ce16bc2a94902e29df4
Blake Tarter <blakertarter@gmail.com>
2022-07-28 11:01:03 -0500

    add some of the variables to the tailwind theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,20 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const kebabcase = require('lodash.kebabcase');
+const variables = require('./src/variables')
+
+const colors = Object.fromEntries(Object
+  .values(variables.color)
+  .flatMap(Object.values)
+  .map(({ path, value }) => [
+    kebabcase(path.join('-')), value
+  ]));
+
 module.exports = {
   content: ["./public/index.html", "./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors
+    },
   },
   plugins: [],
 }
